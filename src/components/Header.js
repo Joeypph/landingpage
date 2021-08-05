b1870af05975953ddf7da95fbd4cee9e3e407c06
José Antonio Lome Paulino <50527508+Joeypph@users.noreply.github.com>
2021-08-05 11:01:42 -0500

    intentando pago desde la pagina subida

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,30 @@
 import React, { useEffect, useState } from "react";
-import {
-  makeStyles,
-  AppBar,
-  IconButton,
-  Toolbar,
-  Collapse,
-} from "@material-ui/core";
+import { makeStyles, AppBar, IconButton, Toolbar, Collapse } from "@material-ui/core";
 import SortIcon from "@material-ui/icons/Sort";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { Link as Scroll } from "react-scroll";
+import Amplify from "aws-amplify";
+import awsconfig from "../aws-exports";
+import { API } from "aws-amplify";
+import { loadStripe } from "@stripe/stripe-js";
+Amplify.configure(awsconfig);
 
 function Header() {
+  const stripePromise = loadStripe("pk_test_RuZc6Tx020hq9t52aCdSCA1e009KAiezMM");
+
+  const redirectToCheckout = async () => {
+    const fetchSession = async () => {
+      const apiName = "stripeAPI";
+      const apiEndpoint = "/checkout";
+
+      const session = await API.post(apiName, apiEndpoint);
+      return session;
+    };
+    const session = await fetchSession();
+    const stripe = await stripePromise;
+    console.log("la sesion", session);
+    stripe.redirectToCheckout({ sessionId: session.id });
+  };
   const classes = useStyles();
 
   const [checked, setChecked] = useState(false);
@@ -20,7 +34,7 @@ function Header() {
   }, []);
 
   return (
-    <div className={classes.root} id="header">
+    <div className={classes.root} id='header'>
       <AppBar className={classes.appbar} elevation={0}>
         <Toolbar className={classes.appbarWrapper}>
           <h1 className={classes.appbarTitle}>
@@ -31,21 +45,18 @@ function Header() {
           </IconButton>
         </Toolbar>
       </AppBar>
-      <Collapse
-        in={checked}
-        {...(checked ? { timeout: 1000 } : {})}
-        collapsedHeight={50}
-      >
+      <Collapse in={checked} {...(checked ? { timeout: 1000 } : {})} collapsedHeight={50}>
         <div className={classes.container}>
           <h1 className={classes.title}>
-            Welcome to <br /> My{" "}
+            Welcome to <br /> My
             <span className={classes.colorText}>Island</span>
           </h1>
-          <Scroll to="place-to-visit" smooth={true}>
+          <Scroll to='place-to-visit' smooth={true}>
             <IconButton className={classes.icon}>
               <ExpandMoreIcon className={classes.goDown} />
             </IconButton>
           </Scroll>
+          <button onClick={redirectToCheckout}>Continue to payment</button>
         </div>
       </Collapse>
     </div>
